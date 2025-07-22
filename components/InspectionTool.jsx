8ae6fa3d07f8f2266e4f9fa5fdf30df33ec43a2c
ReClaim <dustin.stormnation@gmail.com>
2025-07-22 01:40:45 -0500

    Delete components directory

--- components/InspectionTool.jsx
+++
@@ -1,8 +0,0 @@
-export default function InspectionTool() {
-  return (
-    <div style={{ padding: 20 }}>
-      <h1>Reclaim Inspection Tool</h1>
-      <p>Loading your AI-powered roofing inspection…</p>
-    </div>
-  )
-}
